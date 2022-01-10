ea0052dc87ef9a264e20f0b93cf729e44819b4c3
Joanes Garcia <joanesgarcia@outlook.com>
2022-01-10 16:15:56 -0300

    Navigation buttons

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,24 +1,30 @@
 import MovieScore from "components/MovieScore";
+import { Link } from "react-router-dom";
 
-function MoviewCard(){
+function MoviewCard() {
 
     const movie = {
         id: 1,
-        image: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/uzERcfV2rSHNhW5eViQiO9hNiA7.jpg",
-        title: "Duna",
+        //image: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/uzERcfV2rSHNhW5eViQiO9hNiA7.jpg",
+        //title: "Duna",
+        image: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/63O5iixxXSmyOaBas7ek1tkeVra.jpg",
+        title: "Homem Aranha 3",
         count: 2,
         score: 4.5
     };
-    
-    return(
+
+    return (
         <div>
-    <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-    <div className="dsmovie-card-bottom-container">
-        <h3>{movie.title}</h3>
-        <MovieScore />
-        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
-    </div>
-</div>
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="dsmovie-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <MovieScore />
+                <Link to={`/form/${movie.id}`}>
+                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                </Link>
+
+            </div>
+        </div>
     )
 }
 
--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 function Form() {
 
     const movie = {
         id: 1,
-        image: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/uzERcfV2rSHNhW5eViQiO9hNiA7.jpg",
-        title: "Duna",
+        //image: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/uzERcfV2rSHNhW5eViQiO9hNiA7.jpg",
+        //title: "Duna",
+        image: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/63O5iixxXSmyOaBas7ek1tkeVra.jpg",
+        title: "Homem Aranha 3",
         count: 2,
         score: 4.5
     };
@@ -34,7 +37,10 @@ function Form() {
                         <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
                 </form >
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                </Link>
+
             </div >
         </div >
     )
